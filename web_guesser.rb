c54172745f8bb7f42455c49b9514832accd2ca12
Billy <navyosu@gmail.com>
2013-10-14 16:19:26 -0600

    Made secret number not show unless guess is correct and added intro message

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,19 +3,19 @@ require 'sinatra/reloader'
 
   SECRET_NUMBER = rand(100)
 
-  get '/' do 
+  get '/' do
     guess = params["guess"]
     message = check_guess(guess)    
     erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
   end 
 
-  def check_guess(guess)   
+  def check_guess(guess)  
     if guess.to_i > SECRET_NUMBER + 5
       message = "Way too high!"
     elsif guess.to_i < SECRET_NUMBER - 5
       message = "Way too low!"
     elsif guess.to_i == SECRET_NUMBER
-      message = "Correct! You da bomb!"
+      message = "Correct! You da bomb! The secret number is #{SECRET_NUMBER}"
     elsif guess.to_i > SECRET_NUMBER
       message = "Too high!"
     elsif guess.to_i < SECRET_NUMBER
