ff6ef5a75d7a527be08ae4c2dae4efb84a2cef35
Billy <navyosu@gmail.com>
2013-10-14 14:50:27 -0600

    Added if statement to compare guess to random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader'  
 
-random_number = rand(100)
+  SECRET_NUMBER = rand(100)
 
   get '/' do 
-    erb :index, :locals => {:random_number => random_number}
+    guess = params["guess"]
+    message = check_guess(guess)    
+    erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  end 
+
+  def check_guess(guess)   
+    if guess.to_i > SECRET_NUMBER + 5
+      message = "Way too high!"
+    elsif guess.to_i < SECRET_NUMBER - 5
+      message = "Way too low!"
+    elsif guess.to_i == SECRET_NUMBER
+      message = "Correct! You da bomb!"
+    elsif guess.to_i > SECRET_NUMBER
+      message = "Too high!"
+    elsif guess.to_i < SECRET_NUMBER
+      message = "Too low!"
+    end
   end
+    
