963b83ac5da16490212ded9b192f654046c41ad7
Billy <navyosu@gmail.com>
2013-10-13 19:51:10 -0600

    Made server auto reload when rb file changes using reloader.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 random_number = rand(100)
 
   get '/' do 
-    "The secret number is #{random_number}"
+    "The SECRET NUMBER is #{random_number}"
   end
