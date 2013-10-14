2bfcebb6218b597f2144692ec15fced33a0a0f20
Billy <navyosu@gmail.com>
2013-10-14 17:05:45 -0600

    Changed the background color depending on how close to the number you are

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,21 +5,36 @@ require 'sinatra/reloader'
 
   get '/' do
     guess = params["guess"]
-    message = check_guess(guess)    
-    erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
-  end 
+    message = check_guess(guess)  
+    color = check_color(message) 
+    erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
+  end
 
   def check_guess(guess)  
     if guess.to_i > SECRET_NUMBER + 5
-      message = "Way too high!"
+      "Way too high!"
     elsif guess.to_i < SECRET_NUMBER - 5
-      message = "Way too low!"
+      "Way too low!"
     elsif guess.to_i == SECRET_NUMBER
-      message = "Correct! You da bomb! The secret number is #{SECRET_NUMBER}"
+      "Correct! You da bomb! The secret number is #{SECRET_NUMBER}"
     elsif guess.to_i > SECRET_NUMBER
-      message = "Too high!"
+      "Too high!"
     elsif guess.to_i < SECRET_NUMBER
-      message = "Too low!"
+      "Too low!"
+    end
+  end
+
+  def check_color(message)
+    if message == "Way too high!"
+      "red"
+    elsif message == "Way too low!"
+      "red"
+    elsif message == "Correct! You da bomb! The secret number is #{SECRET_NUMBER}"
+      "green"
+    elsif message == "Too high!"
+      "pink"
+    elsif message == "Too low!"
+      "pink"
     end
   end
     
